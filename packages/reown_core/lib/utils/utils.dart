4faed3b38aca533e4f95fb2f7a5ccd7380f4f40d
Alfreedom <00tango.bromine@icloud.com>
2025-01-29 17:52:53 +0100

    Minor changes

--- packages/reown_core/lib/utils/utils.dart
+++ packages/reown_core/lib/utils/utils.dart
@@ -80,7 +80,7 @@ class ReownCoreUtils {
       <String>['Reown-Flutter', sdkVersion].join('-'),
       os,
       id,
-    ].join('/');
+    ].join('/').toLowerCase();
   }
 
   static String formatRelayRpcUrl({
